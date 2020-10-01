c66e22d0f7c390c8d442070bc6b3cea4af870cfa
Nabeel <nsahmed@umich.edu>
2020-10-01 17:00:06 -0400

    Fixed typo in Println

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     for(int i = 1; i <= 10 ; i++) {
       System.out.println("Hello World..." + i);
     }
-    System.out.prinln("This is added code.");
+    System.out.println("This is added code.");
   }
 }
